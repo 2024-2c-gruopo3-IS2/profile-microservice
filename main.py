2079546f4f45991405df93af35ac9a107c2c2d4b
brandonschiffer1 <brandonschiffer11@gmail.com>
2024-10-13 14:47:50 -0300

    Agrego CORS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from controllers.controller import router
 from configs.db import Base, engine
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -9,6 +10,14 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 app.include_router(router, prefix="/profiles", tags=["profiles"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
