091a84861a0a3c7b61175cc672fd349e40a1475d
Martin <mscazzola@fi.uba.ar>
2024-10-17 13:44:19 -0300

    paso las keys a variables de entorno

--- configs/metrics.py
+++ configs/metrics.py
@@ -1,8 +1,10 @@
+import os
 from datadog import initialize
 
 # Configuración de Datadog
 options = {
-    'api_key': 'e120c83170cced6d4857d372c6d40750',
-    'app_key': 'f3e0e8f9c3a9b3f0a3b0c4e5d7a3f1a3'}
+    'api_key': os.getenv('DATADOG_API_KEY'),
+    'app_key': os.getenv('DATADOG_APP_KEY'),
+    }
 
 initialize(**options)
